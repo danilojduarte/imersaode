eb5079815d379b8fc6852e6e83959431d1aaa91e
DaniloDuarte <danilojoseduarte@gmail.com>
2022-09-05 10:26:27 -0300

    Concluída Aula 01 - Variáveis

---
+++ 001 - imersao dev variaveis/var001.js
@@ -0,0 +1,18 @@
+// var nome = "Danilo Duarte"
+// console.log ("Bem Vindo " + nome);
+
+// var notaPrimeiroBimestre = 9
+// var notaFinal = notaPrimeiroBimestre + 1
+
+// console.log(notaFinal)
+
+var notaPrimeiroBimestre = 7
+var notaSegundoBimestre = 5
+var notaTerceiroBimestre = 8
+var notaQuartoBimestre = 6
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4 ;
+
+var notaFinal = notaFinal.toFixed(1);
+
+console.log(notaFinal);
