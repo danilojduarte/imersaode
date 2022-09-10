9829a153f048897a2e81ebad958a648e427d56ec
DaniloDuarte <danilojoseduarte@gmail.com>
2022-09-10 10:44:25 -0300

    Iniciada aula sobre Array

---
+++ 004 - imersao dev aluraflix/script.js
@@ -0,0 +1,16 @@
+
+// Aula de Array
+var listaFilmes = ["Alita", "TopGun 2", "The Matrix"];
+
+// var filme1 = "Alita"
+// var filme2 = "TopGun 2"
+// var filme3 = "The Matrix"
+
+document.write("<p>" + listaFilmes[0] + "</p>");
+document.write("<p>" + listaFilmes[1] + "</p>");
+document.write("<p>" + listaFilmes[2] + "</p>");
+
+
+
+// document.write("<p>" + filme2 + "</p>");
+// document.write("<p>" + filme3 + "</p>");
