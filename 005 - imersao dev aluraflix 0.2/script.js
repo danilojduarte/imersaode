c48a0c6206820a34d147843d56bea7234525c4c4
DaniloDuarte <danilojoseduarte@gmail.com>
2022-09-14 15:40:03 -0300

    Iniciado tabela com JavaScript

--- 005 - imersao dev aluraflix 0.2/script.js
+++ 005 - imersao dev aluraflix 0.2/script.js
@@ -1,16 +1,5 @@
-function adicionarFilme() {
-  var filmeFavorito = document.getElementById("filme").value;
-  if (filmeFavorito.endsWith(".jpg")) {
-    listarFilmesNaTela(filmeFavorito);
-  } else {
-    console.error("Endereço de filme inválido");
-  }
-  document.getElementById("filme").value = "";
-}
+var rafa = { nome: "Rafa", vitorias: 2, empates: 1, derrotas: 1, pontos: 0 };
+var paulo = { nome: "Paulo", vitorias: 1, empates: 1, derrotas: 2, pontos: 0 };
 
-function listarFilmesNaTela(filme) {
-  var elementoFilmeFavorito = "<img src=" + filme + ">";
-  var elementoListaFilmes = document.getElementById("listaFilmes");
-  elementoListaFilmes.innerHTML =
-    elementoListaFilmes.innerHTML + elementoFilmeFavorito;
-}
+console.log(rafa.vitorias);
+console.log(paulo);
