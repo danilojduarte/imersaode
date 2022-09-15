b3d9372bf587c9b9bbb32459a5ffd45c9216867f
DaniloDuarte <danilojoseduarte@gmail.com>
2022-09-15 10:08:10 -0300

    Finalizada tabela JavaScript

--- 006 - imersao dev objetos e tabelas/script.js
+++ 006 - imersao dev objetos e tabelas/script.js
@@ -1,16 +1,54 @@
-function adicionarFilme() {
-  var filmeFavorito = document.getElementById("filme").value;
-  if (filmeFavorito.endsWith(".jpg")) {
-    listarFilmesNaTela(filmeFavorito);
-  } else {
-    console.error("Endereço de filme inválido");
-  }
-  document.getElementById("filme").value = "";
+var rafa = { nome: "Rafa", vitorias: 2, empates: 1, derrotas: 1, pontos: 0 };
+var paulo = { nome: "Paulo", vitorias: 1, empates: 1, derrotas: 2, pontos: 0 };
+
+function calculaPontos(jogador) {
+  var pontos = jogador.vitorias * 3 + jogador.empates;
+  return pontos;
 }
 
-function listarFilmesNaTela(filme) {
-  var elementoFilmeFavorito = "<img src=" + filme + ">";
-  var elementoListaFilmes = document.getElementById("listaFilmes");
-  elementoListaFilmes.innerHTML =
-    elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+rafa.pontos = calculaPontos(rafa);
+paulo.pontos = calculaPontos(paulo);
+
+var jogadores = [rafa, paulo];
+
+function exibeJogadoresNaTela(jogadores) {
+  var elemento = ""
+  for ( var i= 0; i < jogadores.length; i++ ) {
+   elemento += "<tr><td>" + jogadores[i].nome + "</td>"
+   elemento += "<td>" + jogadores[i].vitorias + "</td>"
+   elemento += "<td>" + jogadores[i].empates + "</td>"
+   elemento += "<td>" + jogadores[i].derrotas + "</td>"
+   elemento += "<td>" + jogadores[i].pontos + "</td>"
+   elemento += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>"
+   elemento += "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>"
+   elemento += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>"
+   elemento += "</tr>"  
+  }
+  
+  var tabelaJogadores = document.getElementById("tabelaJogadores")
+  tabelaJogadores.innerHTML = elemento;
 }
+
+exibeJogadoresNaTela(jogadores)
+
+function adicionarVitoria(i) {
+  var jogador = jogadores[i]
+  jogador.vitorias++
+  jogador.pontos = calculaPontos (jogador)
+  exibeJogadoresNaTela(jogadores)
+}
+  function adicionarEmpate(i) {
+  var jogador = jogadores[i]
+  jogador.empates++
+  jogador.pontos = calculaPontos (jogador)
+  exibeJogadoresNaTela(jogadores)
+ } 
+
+ function adicionarDerrota(i) {
+  var jogador = jogadores[i]
+  jogador.derrotas++
+  jogador.pontos = calculaPontos (jogador)
+  exibeJogadoresNaTela(jogadores)
+ } 
+
+ 
