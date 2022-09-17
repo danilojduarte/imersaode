54025e500738477b3cecc4f5483036d7685eb987
DaniloDuarte <danilojoseduarte@gmail.com>
2022-09-17 09:22:59 -0300

    Atualizado arquivo.js
    JavaScript Atualizado

--- 007 - imersao dev Super Trunfo/script.js
+++ 007 - imersao dev Super Trunfo/script.js
@@ -1,6 +1,6 @@
 var carta1 = {
   nome : "Bulbasauro",
-  atribultos: {
+  atributos: {
     ataque: 7,
     defesa: 8,
     magia: 6
@@ -9,7 +9,7 @@ var carta1 = {
 
 var carta2 = {
   nome : "Vegeta",
-  atribultos: {
+  atributos: {
     ataque: 10,
     defesa: 6,
     magia: 8
@@ -18,7 +18,7 @@ var carta2 = {
 
 var carta3 = {
   nome : "Shiryu de Dragão",
-  atribultos: {
+  atributos: {
     ataque: 9,
     defesa: 5,
     magia: 8
@@ -32,7 +32,6 @@ var cartaJogador;
 function sortearCarta() {
   var numeroCartaMaquina = parseInt(Math.random() * 3);
   cartaMaquina = cartas[numeroCartaMaquina];
-  console.log(cartaMaquina);
   
   var numeroCartaJogador = parseInt(Math.random() * 3);
   while (numeroCartaMaquina == numeroCartaJogador) {
@@ -40,6 +39,38 @@ function sortearCarta() {
   }
   cartaJogador = cartas[numeroCartaJogador];
   console.log(cartaJogador);
+  
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  
+  exibirOpcoes()
+}
+
+function exibirOpcoes() {
+  var opcoes = document.getElementById("opcoes");
+  var opcoesTexto = ""
+  
+  for  (var atributo in cartaJogador.atributos) {
+    opcoesTexto += "<input type='radio' 'name=atributo' 'value='" + atributo + "'>" + atributo;
+  }
+  opcoes.innerHTML = opcoesTexto
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributo = document.getElementByName = ("atributo");
+  for ( var i = 0; i < radioAtributo.length; i++) {
+    if (radioAtributo[i].checked == true) {
+      return radioAtributo[i].value
+    }
+  }
+}
+
+
+// Verificar Motivo do valor ter retornado *undefined* Verificar em atributos
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado()
+  console.log(atributoSelecionado)
 }
 
 
